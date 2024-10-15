a67c949ed1cccb21aa2fa50e98cad7612d184291
iarley <iarleysouza615@gmail.com>
2024-10-14 22:29:26 -0300

    atualizando o header

---
+++ Demoday/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import './styles/Header.css'
+
+function Header() {
+
+    return(
+        <header>
+            <div className="logo">
+                <img src="assets/images/logo-branco.png" alt="" />
+            </div>
+            <nav>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+        </header>
+    )
+}
+export default Header
