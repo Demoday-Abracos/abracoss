4a3854fc9cf2027484f465cd9e8f0b339a77b587
iarley <iarleysouza615@gmail.com>
2024-10-22 23:57:46 -0300

    update

--- Demoday/src/components/SobreNos/Descricao.jsx
+++ Demoday/src/components/SobreNos/Descricao.jsx
@@ -1,18 +1,18 @@
 import '../Styles/SobreNos.css'
 
-function Descricao ({imagem,texto,titulo}){
-return (
-    <section className="SobreNos">
-        <div className='imgdescricao'>
-            <img  src={imagem} alt="" />
-        </div>
-        
-        <div>
-            <h1>{titulo}</h1> 
-        <p>{texto}</p>
-        </div>
-    </section>
-) 
+function Descricao({ imagem, texto, titulo }) {
+    return (
+        <section className="SobreNos">
+            <div className='imgdescricao'>
+                <img src={imagem} alt="" />
+            </div>
+
+            <div className='sobreNos-h1'>
+                <h1>{titulo}</h1>
+                <p>{texto}</p>
+            </div>
+        </section>
+    )
 
 
 }
